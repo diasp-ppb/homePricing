f18fc15cef4ae7df6ee1a05eae12e92cd2f568c6
Pedro Dias <pmds3000@hotmail.com>
2018-03-22 19:53:33 +0000

    added deloitte colors

---
+++ mobile/app/themes/Colors.js
@@ -0,0 +1,52 @@
+
+/*
+ Reference: https://www2.deloitte.com/content/dam/Deloitte/sg/Documents/careers/sg-careers-deloitte-sg-50th-anniversary-art-competition-art-colour-palette.pdf
+ */
+
+const DeloittePrimary = {
+    white: '#FFFFFF',
+    black: '#000000',
+    deloitteGreen: '#86BC25',
+    green2:'#C4D600',
+    green4:'#43B02A',
+    green6:'#046A38',
+    green7:'#2c5234',
+    teal5:'#0097A9',
+    blue2:'#62B5E5',
+    blue3:'#00A3E0',
+    blue4:'#0076A8',
+    blue6:'#012169',
+    coolGray2: '#D0D0CE',
+    coolGray4: '#BBBCBC',
+    coolGray7: '#97999B',
+    coolGray9: '#75787B',
+    coolGray11:'#53565A',
+
+
+};
+
+const DeloitteSecondary = {
+    green1: '#E3E48D',
+    green5: '#009A44',
+    teal1: '#DDEFE8',
+    teal2: '#9DD4CF',
+    teal3: '#6FC2B4',
+    teal4: '#00ABAB',
+    teal6: '#007680',
+    teal7: '#004F59',
+    blue1: '#A0DCFF',
+    blue5: '#005587',
+    blue7: '#041E42',
+    coolGray6: '#A7A8AA',
+    coolGray10: '#63666A',
+};
+
+const Colors = {
+
+}
+
+export default {
+    ...DeloittePrimary,
+    ...DeloitteSecondary,
+    ...Colors
+}
