1523ee3d0a6126fc6c9dd79959dbd6a8d4e44727
Diogo Almeida Cunha <diogocunha588@gmail.com>
2018-04-05 18:56:53 +0100

    crawler-mongo connection

--- server/crawler/crawler/uploadData.py
+++ server/crawler/crawler/uploadData.py
@@ -1,2 +1,20 @@
-import pymongo
 
+from pymongo import MongoClient
+from datetime import datetime
+
+connection = 'Starting connection...'
+print(connection)
+
+client = MongoClient('mongodb://mongodb:27017/')
+db = client.test
+
+data = 'wating data data...'
+print(data)
+
+cursor = db.Remax.find()
+
+for document in cursor:
+    print(document)
+
+data = 'RECEIVED data data...'
+print(data)
