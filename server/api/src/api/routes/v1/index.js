3ecd2abbe3aa7454d99243f04618c3c55aa1038c
Diogo Almeida Cunha <diogocunha588@gmail.com>
2018-04-05 12:06:46 +0100

    start connection w mongodb

--- server/api/src/api/routes/v1/index.js
+++ server/api/src/api/routes/v1/index.js
@@ -1,24 +1,28 @@
 const express = require('express');
 const userRoutes = require('./user.route');
 const authRoutes = require('./auth.route');
-
-
+const searchRoutes = require('./search.route');
 const router = express.Router();
 const app = express();
+
 /**
  * GET v1/status
  */
-router.get('/status', (req, res) => res.send('OKlllllllllllllll'));
-
+router.get('/status', (req, res) => res.send('Ok'));
 
 /**
- * GET v1/docs
+ * GET v1/users
  */
-router.use('/docs', express.static('docs'));
-
 router.use('/users', userRoutes);
 
+/**
+ * GET v1/users
+ */
 router.use('/auth', authRoutes);
 
+/**
+ * GET v1/search
+ */
+router.use('/search', searchRoutes);
 
 module.exports = router;
