af0b964620ed6c45cf898c0344b8cb0292259f71
Sara Santos <santos.sara96@gmail.com>
2018-04-14 23:28:03 +0100

    First version of user profile

---
+++ mobile/App/Containers/UserProfileScreen.js
@@ -0,0 +1,90 @@
+import React, { Component } from 'react'
+import { Image, View, AppRegistry, ListView, StyleSheet, TouchableOpacity } from 'react-native'
+import { Images } from '../Themes'
+
+// Native Base
+import { Col, Row, Grid } from 'react-native-easy-grid';
+import { Container, Header, Body, Title, Content, Text, Button, Fab, Icon, ActionSheet } from 'native-base'
+
+// Styles
+import styles from './Styles/UserProfileScreenStyles'
+
+//List Rows
+const rows = [
+    {id: 0, text: 'Home', icon: Images.homeIcon },
+    {id: 1, text: 'Histórico', icon: Images.historyIcon},
+    {id: 2, text: 'Preferências do utilizador', icon: Images.userIcon},
+    {id: 3, text: 'Favoritos', icon: Images.favouriteIcon},
+    {id: 4, text: 'Configurações de conta', icon: Images.settingsIcon},
+    {id: 5, text: 'Ajuda', icon: Images.helpIcon}
+  ]
+  
+  // Row comparison function
+  const rowHasChanged = (r1, r2) => r1.id !== r2.id
+  
+  // DataSource template object
+  const ds = new ListView.DataSource({rowHasChanged})
+
+
+export default class UserProfileScreen extends Component {
+  constructor(props) {
+    super(props);
+    
+  }
+
+  state = {
+    dataSource: ds.cloneWithRows(rows)
+  }
+
+  clickRow = (rowData) => {
+    switch(rowData){
+        case 0:
+            break;
+        case 1: 
+            break;
+        case 2:
+            break;
+        case 3:
+            break;
+        case 4:
+            break;
+        case 5:
+            break;
+        default:
+            break;
+    }
+  }
+
+  renderRow = (rowData) => {
+    return (  
+        <TouchableOpacity key = {rowData.id} onPress = {() => this.clickRow(rowData)}>
+            <View style={styles.listItem}>
+                <Image source = {rowData.icon} style = {styles.listIcons}/>
+                <Text style= {styles.text}>
+                    {rowData.text}
+                </Text>
+            </View>
+        </TouchableOpacity>
+        
+    )
+  }
+
+  render () {
+    return (
+      <Container>
+        <Header>
+          <Body>
+            <Title>Perfil</Title>
+          </Body>
+        </Header>
+        <View style={styles.userInfo}>
+          <Image source={Images.profileIcon} style={styles.icon} />
+        </View>
+          <ListView
+            dataSource={this.state.dataSource}
+            renderRow={this.renderRow}
+            />
+      </Container>
+    )
+  }
+}
--- mobile/App/Navigation/AppNavigation.js
+++ mobile/App/Navigation/AppNavigation.js
@@ -1,15 +1,17 @@
 import { StackNavigator } from 'react-navigation'
 import LaunchScreen from '../Containers/LaunchScreen'
+import UserProfileScreen from '../Containers/UserProfileScreen'
 
 import styles from './Styles/NavigationStyles'
 
 // Manifest of possible screens
 const PrimaryNav = StackNavigator({
-  LaunchScreen: { screen: LaunchScreen }
+  LaunchScreen: { screen: LaunchScreen },
+  UserProfileScreen: {screen: UserProfileScreen}
 }, {
   // Default config for all screens
   headerMode: 'none',
-  initialRouteName: 'LaunchScreen',
+  initialRouteName: 'UserProfileScreen',
   navigationOptions: {
     headerStyle: styles.header
   }
