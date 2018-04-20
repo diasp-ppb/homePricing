b7088817d1c2418a5a48ce42c80787328c34e69c
Beatriz Velho <up201700491@fe.up.pt>
2018-04-20 16:43:14 +0100

    update uploaddata.py file

--- server/crawler/crawler/uploadData.py
+++ server/crawler/crawler/uploadData.py
@@ -1,20 +1,32 @@
-
+# -*- coding: utf-8 -*-
 from pymongo import MongoClient
 from datetime import datetime
+import json
+with open('./imovirtual.json') as json_file:
+    json_data = json.load(json_file)
 
 connection = 'Starting connection...'
 print(connection)
 
-client = MongoClient('mongodb://mongodb:27017/')
+# localhost on port number 27017 connection
+client = MongoClient('mongodb://localhost:27017/')
+
+#choosing database
 db = client.test
-
-data = 'wating data data...'
+data = 'Connection established...'
 print(data)
 
-cursor = db.Remax.find()
+# Remove data before inserting
+rem = "Removing data..."
+print(rem)
+db.Remax.remove()
 
-for document in cursor:
-    print(document)
-
-data = 'RECEIVED data data...'
+# Insert data into mongodb
+db.Remax.insert(json_data)  
+data = 'Database Updatede..'
 print(data)
+
+# Print Data from Database
+#cursor = db.Remax.find()
+#for document in cursor:
+#    print(document)
