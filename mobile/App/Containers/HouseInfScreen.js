af2c3e244c3f09f918754693f3961f6fcb0ac33f
Pedro Dias <pmds3000@hotmail.com>
2018-04-23 22:02:31 +0100

    cleaned code & added slider

--- mobile/App/Containers/HouseInfScreen.js
+++ mobile/App/Containers/HouseInfScreen.js
@@ -1,11 +1,10 @@
 import React, { Component } from 'react'
-import { Image, View, Alert } from 'react-native'
+import { Image, View } from 'react-native'
 import { Images } from '../Themes'
-
+import ImageSlider from 'react-native-image-slider';
 // Native Base
-import { Col, Row, Grid } from 'react-native-easy-grid';
 
-import { Container,Segment, Header, Left, Right, Body, Title, Content, Text, Button, Fab, Icon, ActionSheet } from 'native-base'
+import { Container, Header, Left , Body, Title, Content, Text, Button, Icon} from 'native-base'
 
 // Styles
 import styles from './Styles/HouseInfScreenStyles'
@@ -14,44 +13,43 @@ export default class LaunchScreen extends Component {
   constructor(props) {
     super(props);
     this.state = {
-          area: "",
-          price: "",
-          tipology: "",
-          energy: "none",
-          adress: "",
-          zone: "",
-          condition: "",
-          description: "",
-   };
+      area: "",
+      price: "",
+      tipology: "",
+      energy: "none",
+      adress: "",
+      zone: "",
+      condition: "",
+      description: "",
+    };
   }
 
   /*
   ------------ Start RN communication with /server/api/src/api/routes/v1/index.js ------------
   */
-  componentDidMount(id) {  
+  componentDidMount(id) {
 
-    fetch("http://172.30.29.216:3000/v1/house/5add15005f9c61552a848bf4")
-    .then(function(response){
-       return response.json();
-   })
-   .then(responseJson => {
-      
-       this.setState({
-            area: responseJson.area,
-            price: responseJson.price,
-            tipology: responseJson.tipology,
-            energy: responseJson.energyCertificate,
-            adress: responseJson.adress,
-            bathrooms: responseJson.bathrooms,
-            zone: responseJson.zone,
-            condition: responseJson.condition,
-            description: responseJson.description
-       });
-      
-   })
-   .catch(function(json) {
-       Alert.alert("DEU ERRO");
-   })
+    fetch("http://172.30.29.216:3000/v1/house/5add15005f9c61552a848bf4") //TODO pass this to props
+      .then(function(response){
+        return response.json();
+      })
+      .then(responseJson => {
+
+        this.setState({
+          area: responseJson.area,
+          price: responseJson.price,
+          tipology: responseJson.tipology,
+          energy: responseJson.energyCertificate,
+          adress: responseJson.adress,
+          bathrooms: responseJson.bathrooms,
+          zone: responseJson.zone,
+          condition: responseJson.condition,
+          description: responseJson.description
+        });
+      })
+      .catch(function(json) {
+
+      })
   }
 
   render () {
@@ -66,30 +64,34 @@ export default class LaunchScreen extends Component {
             </Button>
           </Left>
           <Body>
-            <Title style={styles.pageTitle}>House Information</Title>
+          <Title style={styles.pageTitle}>House Information</Title>
           </Body>
         </Header>
 
         <Content>
 
-          <Image style={{width: '100%' , height: 200}}  source={{uri: 'https://imagescdn-gabriels-net.akamaized.net/reno/imagereader.aspx?imageurl=http%3A%2F%2Frealestateadminimages.gabriels.net%2F170%2F170-20170406050356974-972.jpg&option=N&w=640&permitphotoenlargement=false'}}/>
+          <ImageSlider style={{width: '100%' , height: 200}} images={[
+            'https://imagescdn-gabriels-net.akamaized.net/reno/imagereader.aspx?imageurl=http%3A%2F%2Frealestateadminimages.gabriels.net%2F170%2F170-20170406050356974-972.jpg&option=N&w=640&permitphotoenlargement=false',
+            'https://imagescdn-gabriels-net.akamaized.net/reno/imagereader.aspx?imageurl=http%3A%2F%2Frealestateadminimages.gabriels.net%2F170%2F170-20170406050356974-972.jpg&option=N&w=640&permitphotoenlargement=false',
+            'https://imagescdn-gabriels-net.akamaized.net/reno/imagereader.aspx?imageurl=http%3A%2F%2Frealestateadminimages.gabriels.net%2F170%2F170-20170406050356974-972.jpg&option=N&w=640&permitphotoenlargement=false'
+          ]}/>
 
-           <View style={styles.infTab}>
+          <View style={styles.infTab}>
             <Text style={styles.priceText}> {this.state.price} </Text>
             <Text  style={styles.typeText} >House Rent</Text>
           </View>
 
           <View style={styles.box1}>
             <View style={{flex:0.8}}>
-                <Text style={styles.streetText}> {this.state.zone} </Text>
+              <Text style={styles.streetText}> {this.state.zone} </Text>
             </View>
-            <View style={{flex:0.1}}> 
+            <View style={{flex:0.1}}>
               <Image  source={Images.gps} style={{ marginLeft:8, width:32,height:30}}/>
-            </View>      
+            </View>
           </View>
 
           <View style={styles.box1}>
-            <View style={{ flex:0.5}}> 
+            <View style={{ flex:0.5}}>
               <Text style={styles.properties}>Area: </Text>
               <Text style={styles.properties}>Bathrooms: </Text>
               <Text style={styles.properties}>Certificado Ener: </Text>
@@ -97,7 +99,7 @@ export default class LaunchScreen extends Component {
               <Text style={styles.properties}>Condições: </Text>
             </View>
 
-            <View style={{ flex:0.5}}> 
+            <View style={{ flex:0.5}}>
               <Text style={styles.data}> {this.state.area} </Text>
               <Text style={styles.data}>{this.state.bathrooms}</Text>
               <Text style={styles.data}>{this.state.energy}</Text>
@@ -110,7 +112,7 @@ export default class LaunchScreen extends Component {
             <View>
               <Text style={styles.descriptionTitle}> Description </Text>
               <Text style={styles.descriptionText}> {this.state.description} </Text>
-            </View> 
+            </View>
           </View>
 
         </Content>
