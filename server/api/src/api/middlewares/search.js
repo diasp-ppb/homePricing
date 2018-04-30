3681f973920e2b4bc758b68bcfe1e7afbc29884a
Catarina24 <aniratac.correia@gmail.com>
2018-04-30 14:19:50 +0100

    Much more efficient.

--- server/api/src/api/middlewares/search.js
+++ server/api/src/api/middlewares/search.js
@@ -2,12 +2,52 @@ const { googlePlaces, googleMatrix } = require('../services/houseInformationProv
 const { googlePlacesKey, googleMatrixKey } = require('../../config/vars');
 
 // TODO: I'm supposing we get a status: "OK" when returning true
-async function foundResults (key, house, type) {
+async function foundResults(key, house, type) {
     var result = await googlePlaces(key, house, type);
     if (result.status == 'ZERO_RESULTS') {
         return false;
     }
     return true;
+}
+
+function filterHouses(house, filters) {
+
+    return new Promise(function (resolve) {
+        var hospital = true, school = true, shopping = true, transport = true;
+
+        Promise.props({
+            hospital: foundResults(googlePlacesKey, house, 'hospital'),
+            school: foundResults(googlePlacesKey, house, 'school'),
+            shopping: foundResults(googlePlacesKey, house, 'shopping_mall'),
+            transport: foundResults(googlePlacesKey, house, 'bus_station')
+
+        }).then(function (result) {
+            if (hospital && school && shopping && transport) {
+                if (filters.workLocation != null && filters.workDistance != null) {
+                    googleMatrix(googleMatrixKey, house, filters)
+                        .then(function (result) {
+                            if (result.status == "OK") {
+                                var distance = result.rows[0].elements[0].distance.value;
+
+                                // Pass from km to m (units) and adds 999m (eg: 3999m is still 3km)
+                                if (distance <= filters.workDistance * 1000 + 999) {
+                                    resolve(house);
+                                }
+                                else {
+                                    resolve(null);
+                                }
+                            }
+                            else {
+                                resolve(null);
+                            }
+                        })
+                }
+                else {
+                    resolve(house);
+                }
+            }
+        })
+    })
 }
 
 exports.searchHouses = async function (housesToFilter, filters) {
@@ -17,47 +57,12 @@ exports.searchHouses = async function (housesToFilter, filters) {
 
         if (housesToFilter[house].coordinates.length != 0) {
 
-            var add = true; 
+            var result = await filterHouses(housesToFilter[house], filters);
 
-            // Searches if there are any hospitals near
-            if (filters.hospital && add) {
-                add = await foundResults(googlePlacesKey, housesToFilter[house], 'hospital');
-            }
-
-            // Searches if there are any schools near
-            if (filters.school && add) {
-                add = await foundResults(googlePlacesKey, housesToFilter[house], 'school');
-            }
-
-            // Searches if there are any shoppings near
-            if (filters.shopping && add) {
-                add = await foundResults(googlePlacesKey, housesToFilter[house], 'shopping_mall');
-            }
-
-            // Searches if there are any transports near
-            if (filters.transport && add) {
-                add = await foundResults(googlePlacesKey, housesToFilter[house], 'bus_station');
-            }
-
-            if (add) {
-
-                // If a work location and a given distance is specified, it is needed to calculate
-                // the distance between the work location and the house location
-                if (filters.workLocation != null && filters.workDistance != null) {
-                    var result = await googleMatrix(googleMatrixKey, housesToFilter[house], filters);
-                    var distance = result.rows[0].elements[0].distance.value;
-
-                    // Pass from km to m (units) and adds 999m (eg: 3999m is still 3km)
-                    if (distance <= filters.workDistance * 1000 + 999) {
-                        houses.push(housesToFilter[house]);
-                    }
-                }
-                else {
-                    houses.push(housesToFilter[house]);
-                }
+            if (result != null) {
+                houses.push(result);
             }
         }
     }
-
     return houses;
 }
